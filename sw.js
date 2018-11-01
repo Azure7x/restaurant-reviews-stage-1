0a8d85f408566760c05f13185110ef3877ba98c9
Aden Navi <aden7x@gmail.com>
2018-10-31 20:50:45 -0700

    code cleanup

--- sw.js
+++ sw.js
@@ -1,47 +1,17 @@
-let cacheName = 'v4';
-// let cacheFiles = [
-//   'index.html',
-//   'restaurant.html',
-//   '/css/styles.css',
-//   '/js/main.js',
-//   '/js/dbhelper.js',
-//   '/js/restaurant_info.js',
-//   '/data/restaurants.json',
-//   '/img/1.jpg',
-//   '/img/2.jpg',
-//   '/img/3.jpg',
-//   '/img/4.jpg',
-//   '/img/5.jpg',
-//   '/img/6.jpg',
-//   '/img/7.jpg',
-//   '/img/8.jpg',
-//   '/img/9.jpg',
-//   '/img/10.jpg'
-//
-// ];
+// I got a large amount of help for this service worker from
+//https://www.youtube.com/watch?v=ksXwaWHCW6k&t=624s
 
-self.addEventListener('install', function(e) {
-  console.log('serviceworker is installed part three');
 
-  // e.waitUntil(
-  //   caches.open(cacheName)
-  //   .then(cache => {
-  //     console.log('service worker: caching files');
-  //     cache.addAll(cacheFiles);
-  //   })
-  // )
-})
+let cacheName = 'v1';
 
 self.addEventListener('activate', function(e) {
 
-  console.log('service worker activated');
-
+//this makes the acitivate the service worker after it deletes the other caches that that are not current
   e.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cache => {
           if(cache !== cacheName) {
-            console.log('service worker: clearing old cache');
             return caches.delete(cache);
           }
         })
@@ -50,22 +20,20 @@ self.addEventListener('activate', function(e) {
   )
 })
 
-// self.addEventListener('fetch', e => {
-//   console.log('service worker: fetching');
-//   e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-// });
 
 self.addEventListener('fetch', e => {
-  console.log('service worker: fetching');
+
   e.respondWith(
     fetch(e.request)
       .then(res => {
+        //makes a copy of the request and stores it in a cache
         const resClone = res.clone();
         caches.open(cacheName).then(cache => {
           cache.put(e.request, resClone);
         });
         return res;
       })
+      //returns the cached request if offline
       .catch(err => caches.match(e.request).then(res => res))
   );
 });
